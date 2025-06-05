defa7710cc89e8ffb17c320e9ad11256690396ce
Sam Zaydel <szaydel@gmail.com>
2025-06-05 07:51:49 -0700

    Add custom custom ipmi_facts which will only work on Linux systems

---
+++ library/ipmi_facts.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from ansible.module_utils.basic import AnsibleModule
+import subprocess
+
+def extract_ip_address(output):
+    for line in output.splitlines():
+        if line.startswith("IP Address ") and not "Source" in line:
+            return line.split(":")[1][1:]
+
+
+def main():
+    module = AnsibleModule(argument_spec={}, supports_check_mode=True)
+    try:
+        # Run the command
+        res = subprocess.check_output(
+            "/usr/bin/ipmitool lan print 3".split(), universal_newlines=True
+        )
+        # Parse and return structured data
+        ip_address = extract_ip_address(res)
+        facts = {"ipmi_ip_address": ip_address}
+        module.exit_json(changed=False, ansible_facts=facts)
+    except subprocess.CalledProcessError as e:
+        module.fail_json(msg=f"Command failed: {e}")
+    except Exception as e:
+        module.fail_json(msg=f"Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
