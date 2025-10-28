72bb8caccfc4f399b59c89fd2732083f0530b63b
Sam Zaydel <szaydel@gmail.com>
2025-10-28 13:28:02 -0700

    Improve ipmi admin account setup handling

--- library/ipmi_facts.py
+++ library/ipmi_facts.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-from ansible.module_utils.basic import AnsibleModule
+import os
 import subprocess
+
+from ansible.module_utils.basic import AnsibleModule
+
 
 def extract_ip_address(output):
     for line in output.splitlines():
@@ -12,13 +15,16 @@ def extract_ip_address(output):
 def main():
     module = AnsibleModule(argument_spec={}, supports_check_mode=True)
     try:
+        if not os.path.exists("/dev/ipmi0"):
+            facts = {"ipmi_ip_address": "0.0.0.0", "ipmi_present": False}
+            module.exit_json(changed=False, ansible_facts=facts)
         # Run the command
         res = subprocess.check_output(
             "/usr/bin/ipmitool lan print 3".split(), universal_newlines=True
         )
         # Parse and return structured data
         ip_address = extract_ip_address(res)
-        facts = {"ipmi_ip_address": ip_address}
+        facts = {"ipmi_ip_address": ip_address, "ipmi_present": False}
         module.exit_json(changed=False, ansible_facts=facts)
     except subprocess.CalledProcessError as e:
         module.fail_json(msg=f"Command failed: {e}")
