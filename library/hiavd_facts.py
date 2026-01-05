421790a626a8c204dd84a35f13137c3f959d55f4
Sam Zaydel <szaydel@gmail.com>
2026-01-05 07:56:16 -0800

    Pool import must be skipped if pool is already in a resource group

--- library/hiavd_facts.py
+++ library/hiavd_facts.py
@@ -2,9 +2,9 @@
 import json
 import os
 import subprocess
+from typing import Any, Dict
 
 from ansible.module_utils.basic import AnsibleModule
-
 
 DEFAULT_CONFIGFILE = "/etc/racktop/hiavd/hiavd.conf"
 DEFAULT_STATEFILE = "/etc/racktop/hiavd/serialized.dat"
@@ -15,24 +15,71 @@ class MissingRevisionError(Exception):
     pass
 
 
+def get_conf_subgroup(statefile: str, subgroup: str) -> Dict[str, Any]:
+    """Fetches the named subgroup from the serialized configuration."""
+    d = dict()
+    state = statefile_to_dict(statefile)
+    cluster = state.get("Cluster")
+    if cluster:
+        d = cluster.get(subgroup)
+    return d
+
+
+def statefile_to_dict(statefile: str) -> Dict[str, Any]:
+    """Converts serialized statefile to a native dict object."""
+    try:
+        with open(statefile, "rb") as fp:
+            current_state = json.load(fp)
+    except FileNotFoundError:
+        return dict()
+    return current_state
+
+
+def resource_groups(statefile: str) -> Dict[str, Any]:
+    """Fetches the ResourceGroups object out of the serialized configuration."""
+    return get_conf_subgroup(statefile, "ResourceGroups")
+
+
+def pools(statefile: str) -> Dict[str, Any]:
+    """Fetches the Pools object out of the serialized configuration."""
+    return get_conf_subgroup(statefile, "Pools")
+
+
+def pool_info_by_name(statefile: str) -> Dict[str, Any]:
+    """
+    Generates a dict where keys are pool names and values are details for the
+    given pool.
+    """
+    p = pools(statefile)
+    new_p = dict()
+    for k, v in p.items():
+        poolname = v.get("CachedName")
+        if not poolname:
+            continue
+        new_p[poolname] = v
+    return new_p
+
+
 def statefile_is_missing(statefile: str) -> bool:
+    """Returns True if there is no statefile on the system."""
     return not os.path.exists(statefile)
 
 
 def revision_is_initial(statefile: str) -> bool:
-    with open(statefile, "rb") as fp:
-        state = json.load(fp)
-        cluster = state.get("Cluster")
-        if cluster:
-            return cluster.get("Revision") == DEFAULT_REVISION_ID
+    state = statefile_to_dict(statefile)
+    cluster = state.get("Cluster")
+    if cluster:
+        return cluster.get("Revision") == DEFAULT_REVISION_ID
     raise MissingRevisionError("no revision field found in the state file")
 
 
 def statefile_missing_or_initial(statefile: str) -> bool:
+    """Returns True if no configuration or initial configuration."""
     return statefile_is_missing(statefile) or revision_is_initial(statefile)
 
 
 def no_cluster_nodes_defined(configfile: str):
+    """Returns True if there are no nodes defined in the cluster."""
     with open(configfile, "rb") as f:
         for line in f.readlines():
             if line.startswith(b"[[ClusterNodes]]"):
@@ -63,9 +110,10 @@ def main():
         _, version = res.split()
         hiavd_facts = {
             "hiavd": {
-                "version": version,
                 "new_configuration": no_cluster_nodes_defined(configfile)
                 or statefile_missing_or_initial(statefile),
+                "pools": pool_info_by_name(statefile),
+                "version": version,
             }
         }
         module.exit_json(changed=False, ansible_facts=hiavd_facts)
